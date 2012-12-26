ed0abc4e4939b1d31043c9bc8c2adcace88c909b
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2012-12-26 15:10:59 -0800

    Added pages

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,57 +1,71 @@
-﻿require(["dojo/on", "esri/map"], function (on) {
+﻿/*global require, jQuery */
+(function ($) {
 	"use strict";
-	var map, basemap;
+	require(["dojo/on", "esri/map"], function (on) {
+		var map, basemap;
 
-	function resizeMap(e) {
-		/// <summary>Resizes the map and container to fill screen.</summary>
-		/// <param name="e" type="Event">Event object</param>
-		var screenHeight, titleHeight, newHeight, mapContainer, padding;
+		function resizeMap(e) {
+			/// <summary>Resizes the map and container to fill screen.</summary>
+			/// <param name="e" type="Event">Event object</param>
+			var screenHeight, titleHeight, newHeight, mapContainer, padding;
 				
 
-		function getPaddingHeight() {
-			var pxRe = /^\d+(?=px)/, mapContainer = $("#mapContainer"), top, bottom;
-			top = mapContainer.css("padding-top").match(pxRe);
-			bottom = mapContainer.css("padding-bottom").match(pxRe);
+			function getPaddingHeight() {
+				var pxRe = /^\d+(?=px)/, mapContainer = $("#mapContainer"), top, bottom;
+				top = mapContainer.css("padding-top").match(pxRe);
+				bottom = mapContainer.css("padding-bottom").match(pxRe);
 
-			return top && bottom ? Number(top[0]) + Number(bottom[0]) : null;
-		}
+				return top && bottom ? Number(top[0]) + Number(bottom[0]) : null;
+			}
 
-		screenHeight = e ? e.target.innerHeight : $("html")[0].clientHeight;
-		titleHeight = $("#header")[0].clientHeight;
+			screenHeight = e ? e.target.innerHeight : $("html")[0].clientHeight;
+			titleHeight = $("#header")[0].clientHeight;
 				
-		padding = getPaddingHeight();
+			padding = getPaddingHeight();
 
-		newHeight = screenHeight - titleHeight - padding;
+			newHeight = screenHeight - titleHeight - padding;
 
-		$("#map").css("height", newHeight);
+			$("#map").css("height", newHeight);
 
-		map.resize();
-	}
-
-	function moveToCurrentPosition(location) {
-		var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
-		map.centerAndZoom(pt, 16);
-	}
-
-	function handleLocationError(error) {
-		console.error(error);
-	}
-
-	map = new esri.Map("map");
-
-	////// TODO: for debugging, set map as a global variable.  Remove when moved to production.
-	////window.esriMap = map;
-
-	basemap = new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer");
-	map.addLayer(basemap);
-
-	on(window, "resize", resizeMap);
-
-	dojo.connect(map, "onLoad", function () {
-		resizeMap();
-
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(moveToCurrentPosition, handleLocationError);
+			map.resize();
+			map.reposition();
 		}
+
+		function moveToCurrentPosition(location) {
+			var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
+			map.centerAndZoom(pt, 16);
+		}
+
+		function handleLocationError(error) {
+			console.error(error);
+		}
+
+		map = new esri.Map("map");
+
+		////// TODO: for debugging, set map as a global variable.  Remove when moved to production.
+		////window.esriMap = map;
+
+		basemap = new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer");
+		map.addLayer(basemap);
+
+		on(window, "resize", resizeMap);
+
+		dojo.connect(map, "onLoad", function () {
+			resizeMap();
+
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(moveToCurrentPosition, handleLocationError);
+			}
+
+			$(document).bind("pagechange", function (event, data) {
+				var id;
+				if (data.toPage) {
+					id = data.toPage.attr("id");
+					if (id === "mainpage") {
+						resizeMap();
+					}
+				}
+			});
+		});
 	});
-});
+}(jQuery));
