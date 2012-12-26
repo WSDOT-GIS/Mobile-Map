6ed66a4c0f3970395b932f34397f29d36061ea19
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2012-12-26 12:38:03 -0800

    Split CSS and JavaScript from index.html into separate files.

---
+++ Scripts/index.js
@@ -0,0 +1,57 @@
+﻿require(["dojo/on", "esri/map"], function (on) {
+	"use strict";
+	var map, basemap;
+
+	function resizeMap(e) {
+		/// <summary>Resizes the map and container to fill screen.</summary>
+		/// <param name="e" type="Event">Event object</param>
+		var screenHeight, titleHeight, newHeight, mapContainer, padding;
+				
+
+		function getPaddingHeight() {
+			var pxRe = /^\d+(?=px)/, mapContainer = $("#mapContainer"), top, bottom;
+			top = mapContainer.css("padding-top").match(pxRe);
+			bottom = mapContainer.css("padding-bottom").match(pxRe);
+
+			return top && bottom ? Number(top[0]) + Number(bottom[0]) : null;
+		}
+
+		screenHeight = e ? e.target.innerHeight : $("html")[0].clientHeight;
+		titleHeight = $("#header")[0].clientHeight;
+				
+		padding = getPaddingHeight();
+
+		newHeight = screenHeight - titleHeight - padding;
+
+		$("#map").css("height", newHeight);
+
+		map.resize();
+	}
+
+	function moveToCurrentPosition(location) {
+		var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
+		map.centerAndZoom(pt, 16);
+	}
+
+	function handleLocationError(error) {
+		console.error(error);
+	}
+
+	map = new esri.Map("map");
+
+	////// TODO: for debugging, set map as a global variable.  Remove when moved to production.
+	////window.esriMap = map;
+
+	basemap = new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer");
+	map.addLayer(basemap);
+
+	on(window, "resize", resizeMap);
+
+	dojo.connect(map, "onLoad", function () {
+		resizeMap();
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(moveToCurrentPosition, handleLocationError);
+		}
+	});
+});
